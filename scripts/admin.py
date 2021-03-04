d92910376d07e3351e4c25d88908445c667cdef8
Geoff Thomas <admiralgt@gmail.com>
2021-03-04 23:39:24 +0000

    Make ScriptVersion editable as admin

--- scripts/admin.py
+++ scripts/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Script
+from .models import Script, ScriptVersion
 
 admin.site.register(Script)
+admin.site.register(ScriptVersion)
