8378ba21fee6207ed6f7472f80d6751c90b00851
Geoff Thomas <admiralgt@gmail.com>
2021-03-05 21:19:46 +0000

    Script View now displays the selected script version

--- scripts/views.py
+++ scripts/views.py
@@ -12,7 +12,11 @@ class ScriptsView(generic.ListView):
 class ScriptView(generic.DetailView):
     template_name = "script.html"
     model = models.Script
-    
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if "sel_name" in self.request.GET:
+            context['script_version'] = self.object.versions.get(version=self.request.GET["sel_name"])
+        else:
+            context['script_version'] = self.object.versions.latest('pk')
+        return context
