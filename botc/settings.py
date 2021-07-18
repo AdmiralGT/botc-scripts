950e3c97b9fb3ba6f761d7054245c33b9e18eca0
Geoff Thomas <admiralgt@gmail.com>
2021-07-18 18:11:58 +0100

    Provided ability to include and exclude characters

--- botc/settings.py
+++ botc/settings.py
@@ -82,9 +82,14 @@ WSGI_APPLICATION = "botc.wsgi.application"
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "botcdb",
+        "HOST": "localhost",
+        "USER": "localuser",
+        "PASSWORD": "localpassword",
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
