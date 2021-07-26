39f9a8036eaa0f9533a26fd58461b4a0f5189b74
Geoff Thomas <admiralgt@gmail.com>
2021-07-26 18:53:21 +0100

    Allow the admin panel to see the creation time

--- botc/settings.py
+++ botc/settings.py
@@ -115,10 +115,11 @@ TIME_ZONE = "UTC"
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
 
+DATETIME_FORMAT = "c"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
