b12f79b50ffcabf3f928caa191c5a152f66e87ca
Geoff Thomas <admiralgt@gmail.com>
2021-12-21 02:19:29 +0000

    Move local usage to use local secrets

--- botc/wsgi.py
+++ botc/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", os.environ.get("DJANGO_SETTINGS", "botc.settings")
+    "DJANGO_SETTINGS_MODULE", os.environ.get("DJANGO_SETTINGS", "botc.local")
 )
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", os.environ.get("DJANGO_SETTINGS", "botc.settings")
+        "DJANGO_SETTINGS_MODULE", os.environ.get("DJANGO_SETTINGS", "botc.local")
     )
     try:
         from django.core.management import execute_from_command_line
