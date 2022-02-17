054071e5d79fe484b6f5e8e065fca9bba2b531cb
Geoff Thomas <admiralgt@gmail.com>
2022-02-17 03:02:54 +0000

    Don't show newer changes than ones shown and re-architect so I can test function

--- scripts/views.py
+++ scripts/views.py
@@ -52,44 +52,54 @@ class UserScriptsListView(SingleTableMixin, FilterView):
     ordering = ["-pk"]
 
 
+def get_json_additions(old_json, new_json):
+    for old_id in old_json:
+        if old_id["id"] == "_meta":
+            continue
+        for new_id in new_json:
+            if new_id["id"] == "_meta":
+                continue
+
+            if old_id == new_id:
+                new_json.remove(new_id)
+
+    for new_id in new_json:
+        if new_id["id"] == "_meta":
+            new_json.remove(new_id)
+            break
+
+    return new_json
+
+
 class ScriptView(generic.DetailView):
     template_name = "script.html"
     model = models.Script
 
-    def get_json_additions(self, old_json, new_json):
-        for old_id in old_json:
-            if old_id["id"] == "_meta":
-                continue
-            for new_id in new_json:
-                if new_id["id"] == "_meta":
-                    continue
-
-                if old_id == new_id:
-                    new_json.remove(new_id)
-
-        for new_id in new_json:
-            if new_id["id"] == "_meta":
-                new_json.remove(new_id)
-                break
-
-        return new_json
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
+        if "sel_name" in self.request.GET:
+            context["script_version"] = self.object.versions.get(
+                version=self.request.GET["sel_name"]
+            )
+        else:
+            context["script_version"] = self.object.versions.last()
+        curr_version = context["script_version"].version
 
         changes = {}
         diff_script_version = None
         for script_version in reversed(self.object.versions.all()):
+
+            # If we're looking at an older script, don't show changes future to that.
+            if script_version.version.internal_integer > curr_version.internal_integer:
+                continue
+
             if diff_script_version:
                 changes[diff_script_version.version] = {}
-                changes[diff_script_version.version][
-                    "additions"
-                ] = self.get_json_additions(
+                changes[diff_script_version.version]["additions"] = get_json_additions(
                     script_version.content.copy(), diff_script_version.content.copy()
                 )
-                changes[diff_script_version.version][
-                    "deletions"
-                ] = self.get_json_additions(
+                changes[diff_script_version.version]["deletions"] = get_json_additions(
                     diff_script_version.content.copy(), script_version.content.copy()
                 )
                 changes[diff_script_version.version][
@@ -99,12 +109,6 @@ class ScriptView(generic.DetailView):
 
         context["changes"] = changes
 
-        if "sel_name" in self.request.GET:
-            context["script_version"] = self.object.versions.get(
-                version=self.request.GET["sel_name"]
-            )
-        else:
-            context["script_version"] = self.object.versions.last()
         return context
 
 
