982984a8762abcc5c72e0960cb05894a025690ab
Ciarán Walsh <git@ciaranwal.sh>
2025-10-11 17:05:14 +0100

    Rename field alias

--- scripts/queries.py
+++ scripts/queries.py
@@ -6,11 +6,11 @@ from django.db import connection
 def get_all_script_character_counts():
     with connection.cursor() as cursor:
         cursor.execute("""
-            SELECT character_type->>'id' AS character, COUNT(1) AS count
+            SELECT character_instance->>'id' AS character, COUNT(1) AS count
             FROM "scripts_scriptversion"
-            CROSS JOIN LATERAL jsonb_array_elements(content) character_type
-            WHERE character_type->>'id' != '_meta'
-            GROUP BY character_type->>'id'
+            CROSS JOIN LATERAL jsonb_array_elements(content) character_instance
+            WHERE character_instance->>'id' != '_meta'
+            GROUP BY character_instance->>'id'
             ORDER BY count DESC
         """)
         return dict(cursor.fetchall())
